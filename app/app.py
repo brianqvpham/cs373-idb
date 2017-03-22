dee53508f426f04bb42d234986d7aef5652a29c2
Weston Selleck <wwselleck@gmail.com>
2017-03-21 22:48:27 -0500

    Modularize app, create initdb script

--- app/app.py
+++ app/app.py
@@ -1,13 +1,19 @@
-from flask import Flask
+from flask import Flask, Blueprint
 from models import db
 
-app = Flask(__name__)
-db.init_app(app)
+blueprint = Blueprint('blueprint', __name__)
 
-@app.route('/')
+@blueprint.route('/')
 def index():
     return "Hello World!"
 
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(blueprint)
+    db.init_app(app)
+    return app
+
 if __name__ == "__main__":
+    app = create_app()
     app.run()
 
--- app/models.py
+++ app/models.py
@@ -2,13 +2,38 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Source():
-    pass
+article_country_table = db.Table('article_country',
+        db.Column('article_id', db.Integer, db.ForeignKey('article.id')),
+        db.Column('country_id', db.Integer, db.ForeignKey('country.id')),
+)
 
-class Article():
-    pass
+class Source(db.Model):
+    __tablename__ = 'source'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    description = db.Column(db.String)
+    url = db.Column(db.String)
+    logoUrl = db.Column(db.String)
 
-class Country():
-    pass
+class Article(db.Model):
+    __tablename__ = 'article'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    author = db.Column(db.String)
+    description = db.Column(db.String)
+    url = db.Column(db.String)
+    imageUrl = db.Column(db.String)
 
+    countries = db.relationship('Country',
+            secondary=article_country_table,
+            back_populates='articles')
+
+class Country(db.Model):
+    __tablename__ = 'country'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+    articles = db.relationship('Country',
+            secondary=article_country_table,
+            back_populates='countries')
 
