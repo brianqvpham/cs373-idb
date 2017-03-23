68e85c82662666e8abc102f3d8bf75e1b312baf2
Weston Selleck <wwselleck@gmail.com>
2017-03-22 23:14:16 -0500

    Static data

--- app/models.py
+++ app/models.py
@@ -3,12 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 article_country_table = db.Table('article_country',
-                                 db.Column('article_id', db.Integer,
-                                           db.ForeignKey('article.id')),
-                                 db.Column('country_id', db.Integer,
-                                           db.ForeignKey('country.id')),
-                                 )
-
+        db.Column('article_id', db.Integer, db.ForeignKey('article.id')),
+        db.Column('country_id', db.Integer, db.ForeignKey('country.id')),
+)
 
 class Source(db.Model):
     __tablename__ = 'source'
@@ -25,13 +22,13 @@ class Source(db.Model):
     def __repr__(self):
         return '<Source {0}>'.format(self.country)
 
-
 class Article(db.Model):
     __tablename__ = 'article'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     author = db.Column(db.String)
     description = db.Column(db.String)
+    publishDate = db.Column(db.String)
     url = db.Column(db.String)
     imageUrl = db.Column(db.String)
 
@@ -39,12 +36,11 @@ class Article(db.Model):
     source = db.relationship('Source', back_populates='articles')
 
     countries = db.relationship('Country',
-                                secondary=article_country_table,
-                                back_populates='articles')
+            secondary=article_country_table,
+            back_populates='articles')
 
     def __repr__(self):
         return '<Article {0}>'.format(self.title)
-
 
 class Country(db.Model):
     __tablename__ = 'country'
@@ -56,10 +52,11 @@ class Country(db.Model):
     flagUrl = db.Column(db.String)
 
     articles = db.relationship('Article',
-                               secondary=article_country_table,
-                               back_populates='countries')
+            secondary=article_country_table,
+            back_populates='countries')
 
     sources = db.relationship('Source', back_populates='country')
 
     def __repr__(self):
         return '<Country {0}>'.format(self.name)
+
