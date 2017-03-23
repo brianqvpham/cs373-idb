49b71d09e36e84f39e777e397dea4362fbc8b983
Weston Selleck <wwselleck@gmail.com>
2017-03-23 18:49:29 -0500

    A bunch of template stuff

--- app/blueprints/articles.py
+++ app/blueprints/articles.py
@@ -4,12 +4,13 @@ from blueprints.static_data import static_data
 articles_bp = Blueprint('articles', __name__,)
 
 
-@articles_bp.route('/articles/', defaults={'page' :'articles'})
-@articles_bp.route('/articles/<page>')
-def show_article(page):
-    # if page == 'articles':
-    #     return render_template('%s.html' % page)
-    # else:
-    print(static_data['articles'])
-        # Change this when creating separate pages for articles
-    return render_template('articles.html', sources=static_data['articles'])
+@articles_bp.route('/articles/')
+@articles_bp.route('/articles/<id>')
+def show_article(id=None):
+    if (not id):
+        data = static_data['articles']
+        return render_template('articles.html', articles=data)
+    else:
+        # Find correct article in articles list
+        data = list(filter(lambda x: x["id"] == id, static_data['articles']))[0]
+        return render_template('article.html', article=data)
--- app/blueprints/countries.py
+++ app/blueprints/countries.py
@@ -3,13 +3,15 @@ from blueprints.static_data import static_data
 
 countries_bp = Blueprint('countries', __name__,)
 
-
-@countries_bp.route('/countries/', defaults={'page' :'countries'})
-@countries_bp.route('/countries/<page>')
-def show_country(page):
-    # if page == 'countries':
-    #     return render_template('%s.html' % page)
-    # else:
-    print(static_data['countries'])
-        # Change this when creating separate pages for countries
-    return render_template('countries.html', sources=static_data['countries'])
+@countries_bp.route('/countries/')
+@countries_bp.route('/countries/<id>')
+def show_country(id=None):
+    if (not id):
+        data = static_data['countries']
+        return render_template('countries.html', countries=data)
+    else:
+        # Find correct country in countrys list
+        country = list(filter(lambda x: x["id"] == id, static_data['countries']))[0]
+        articles = list(filter(lambda x: x["id"] in country['articles'], static_data['articles']))
+        sources = list(filter(lambda x: x["id"] in country['sources'], static_data['sources']))
+        return render_template('country.html', country=country, articles=articles, sources=sources)
