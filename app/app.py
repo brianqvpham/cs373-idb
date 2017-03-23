5eee02cb62dc0adc5f34662553ae5d47d73097ff
Brian <brianqvpham@hotmail.com>
2017-03-22 20:34:54 -0500

    Added articles blueprint.

--- app/app.py
+++ app/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, Blueprint, render_template
 from models import db
+from blueprints.articles import articles_bp
 
 blueprint = Blueprint('blueprint', __name__)
 
 @blueprint.route('/')
-@blueprint.route('/<page>')
+#@blueprint.route('/<page>')
 def index(page='home'):
     data = {}
     return render_template('{0}.html'.format(page), **data)
@@ -12,6 +13,7 @@ def index(page='home'):
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(blueprint)
+    app.register_blueprint(articles_bp)
     db.init_app(app)
     return app
 
