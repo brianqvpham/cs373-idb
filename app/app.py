00f70f4e14847306ef4e9fbf7e5a6ab40acfed6e
Brian <brianqvpham@hotmail.com>
2017-03-31 15:42:59 -0500

    Added route to /tests/ to run unit tests.

--- app/app.py
+++ app/app.py
@@ -5,6 +5,9 @@ from blueprints.organizations import organizations_bp
 from blueprints.countries import countries_bp
 from blueprints.static_data import static_data
 
+import os
+import subprocess
+
 blueprint = Blueprint('blueprint', __name__)
 
 
@@ -17,6 +20,19 @@ def index():
 def about():
     return render_template('about.html')
 
+@blueprint.route('/tests/')
+def tests():
+    test_script = 'tests.py'
+    script_dir = os.path.dirname(__file__)
+    try:
+        result = subprocess.run(['python3.5', 
+                                os.path.join(script_dir, test_script)],
+                                stdout = subprocess.PIPE, 
+                                stderr=subprocess.STDOUT, check=True).stdout
+    except subprocess.CalledProcessError as e:
+        result = e.output
+    return result.decode('utf-8')
+        
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(blueprint)
